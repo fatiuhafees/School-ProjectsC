2f0827d3f4544d06d08ec1ad459471a8d64d3e12
Fatiu Kehinde Hafees <121571697+fatiuhafees@users.noreply.github.com>
2023-08-07 16:01:35 +0100

    Order#List_L-S.c

--- Order#List_L-S.c
+++ Order#List_L-S.c
@@ -3,6 +3,7 @@ int main()
 { 
    int array[] = {7, 2, 6, 3, 5};
    int size = sizeof(array)/sizeof(array[0]);
+   //order biggest to samllest, print values in array
    sort(array, size);
    printArray(array, size);
  
@@ -11,10 +12,13 @@ int main()
 
 void sort(int array[], int size)
 {
+// nested loop is used to scan through the array 
    for(int i = 0; i < size - 1; i++)
    {
       for(int j = 0; j < (size -i )- 1; j++)
       {
+         //if number to the right is bigger switch places
+         //array[j+1] lets us look at the number to the right of [J]
          if(array[j] < array[j+1]) 
          {
             int temp = array[j];
